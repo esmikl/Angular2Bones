75e71f4e3277b9c6a5fbd8f9ccfe8010e046fede
mikle <emikl@glhec.org>
2017-02-01 14:31:37 -0600

    Finish simple Angular 2 app by adding form

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,68 +3,23 @@ import { User } from './shared/models/user';
 
 @Component ({
     selector: 'my-app',
-    template: `
-        <header>
-            <nav class="navbar navbar-inverse">
-                <div class="navbar-header">
-                    <a href="/" class="navbar-brand">My Angular 2 App!</a>
-                </div>
-            </nav>
-        <header>
-
-        <main>
-			<div class="row">
-				<div class="col-sm-4">
-					<div *ngIf="users">
-						<ul class="list-group users-list" *ngIf="users">
-							<li class="list-group-item" *ngFor="let user of users"
-                            (click)="selectUser(user)"
-                            [class.active]="user === activeUser">
-								{{ user.name }} ({{ user.username }})
-							</li>
-						</ul>
-					</div>	
-				</div>
-                <div class="col-sm-8">
-                    <div class="jumbotron" *ngIf="activeUser">
-                        <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>
-                    </div>
-
-                    <div class="jumbotron gocrazy" *ngIf="!activeUser">
-                        <span class="glyphicon glyphicon-hand-left"></span>
-                        <h2>Choose a User</h2>
-                    </div>
-                </div>
-			</div>
-        </main>
-
-        <footer class="text-center">
-            <small>Copyright &copy; 2017</small>
-        </footer>
-    `,
-    styles: [`
-        .users-list li {
-            cursor: pointer;
-        }
-        .jumbotron .glyphicon {
-            font-size: 80px;
-        }
-        .gocrazy {
-            background: red;
-            color: #FFF;
-        }
-    `]
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
 export class AppComponent{
     message: string = 'Hello!';
     users: User[] = [
         {id: 25, name: 'Emily', username: 'esmikl'},
         {id: 26, name: 'Nick', username: 'whatnicktweets'},
-        {id: 27, name: 'Holly', username: 'hollylawly'},
+        {id: 27, name: 'Holly', username: 'hollylawly'}
     ];
     activeUser: User;
 
     selectUser(user) {
         this.activeUser = user;
+    }
+
+    onUserCreated(event) {
+        this.users.push(event.user);
     }
 }
--- dist/app.component.js
+++ dist/app.component.js
@@ -12,19 +12,22 @@ var AppComponent = (function () {
         this.users = [
             { id: 25, name: 'Emily', username: 'esmikl' },
             { id: 26, name: 'Nick', username: 'whatnicktweets' },
-            { id: 27, name: 'Holly', username: 'hollylawly' },
+            { id: 27, name: 'Holly', username: 'hollylawly' }
         ];
     }
     AppComponent.prototype.selectUser = function (user) {
         this.activeUser = user;
+    };
+    AppComponent.prototype.onUserCreated = function (event) {
+        this.users.push(event.user);
     };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n        <header>\n            <nav class=\"navbar navbar-inverse\">\n                <div class=\"navbar-header\">\n                    <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n                </div>\n            </nav>\n        <header>\n\n        <main>\n\t\t\t<div class=\"row\">\n\t\t\t\t<div class=\"col-sm-4\">\n\t\t\t\t\t<div *ngIf=\"users\">\n\t\t\t\t\t\t<ul class=\"list-group users-list\" *ngIf=\"users\">\n\t\t\t\t\t\t\t<li class=\"list-group-item\" *ngFor=\"let user of users\"\n                            (click)=\"selectUser(user)\"\n                            [class.active]=\"user === activeUser\">\n\t\t\t\t\t\t\t\t{{ user.name }} ({{ user.username }})\n\t\t\t\t\t\t\t</li>\n\t\t\t\t\t\t</ul>\n\t\t\t\t\t</div>\t\n\t\t\t\t</div>\n                <div class=\"col-sm-8\">\n                    <div class=\"jumbotron\" *ngIf=\"activeUser\">\n                        <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>\n                    </div>\n\n                    <div class=\"jumbotron gocrazy\" *ngIf=\"!activeUser\">\n                        <span class=\"glyphicon glyphicon-hand-left\"></span>\n                        <h2>Choose a User</h2>\n                    </div>\n                </div>\n\t\t\t</div>\n        </main>\n\n        <footer class=\"text-center\">\n            <small>Copyright &copy; 2017</small>\n        </footer>\n    ",
-        styles: ["\n        .users-list li {\n            cursor: pointer;\n        }\n        .jumbotron .glyphicon {\n            font-size: 80px;\n        }\n        .gocrazy {\n            background: red;\n            color: #FFF;\n        }\n    "]
+        templateUrl: './app/app.component.html',
+        styleUrls: ['./app/app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
